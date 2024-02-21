95fa1df7a98fbf91b248cb0cfee6e665887ca61b
mzw86020424 <mzw86020424@gmail.com>
2024-02-22 08:26:44 +0900

    jestの設定ファイルを作成

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// この@typeのコメントはエディターに型情報を与えるためのものです。これを書いておくことで、エディター上で入力補完が効くようになります。
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
